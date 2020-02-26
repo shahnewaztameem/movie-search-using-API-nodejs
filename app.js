6765f3c42bf235c11d38838cd688886c28e1fc7a
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-02-26 17:02:49 +0600

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,18 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render("search");
+app.get("/", (req, res) => {
+  res.render("search");
 });
 app.get("/result", (req, res) => {
-    var searchTerm = req.query.search;
-    var url = 'http://www.omdbapi.com/?s=' + searchTerm + '&apikey=thewdb';
-  request(
-    url,
-    (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        var results = JSON.parse(body);
-        res.render('results', {results: results});
-      }
+  var searchTerm = req.query.search;
+  var url = "http://www.omdbapi.com/?s=" + searchTerm + "&apikey=thewdb";
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      var results = JSON.parse(body);
+      res.render("results", { results: results });
     }
-  );
+  });
 });
 app.listen(3000, function() {
   console.log("App has been started on port 3000!");
